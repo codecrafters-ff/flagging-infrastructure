6d3f50e690f1a1b523a5998816e3d55cd3b956ca
OngeziweM5 <ongeziwem@betsoftware.com>
2025-10-30 23:48:48 +0200

    interminent ssh issues fix

--- environments/development/network.tf
+++ environments/development/network.tf
@@ -12,6 +12,5 @@ module "network" {
   public_subnet_cidr_b = "10.10.2.0/24"
   az_a                 = "af-south-1a"
   az_b                 = "af-south-1b"
-  allowed_ssh_cidrs    = var.allowed_ssh_cidrs
   allowed_api_cidrs    = var.allowed_api_cidrs
 }
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,6 +1,6 @@
 ##############################################
 # NETWORK MODULE VARIABLES
-\# modules/network/variables.tf
+# modules/network/variables.tf
 ##############################################
 
 variable "name" { type = string }
