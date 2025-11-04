e2977da5536b5f91f082d6536697941601084b7c
OngeziweM5 <ongeziwem@betsoftware.com>
2025-11-04 14:23:58 +0200

    Upping ec2 instance resource to run sql-server (require 2GB RAM and more)

--- modules/compute-ec2/variables.tf
+++ modules/compute-ec2/variables.tf
@@ -16,7 +16,7 @@ variable "environment" {
 variable "instance_type" {
   description = "EC2 instance type for compute environment"
   type        = string
-  default     = "t3.micro"
+  default     = "t3.medium"
 }
 
 variable "subnet_ids" {
