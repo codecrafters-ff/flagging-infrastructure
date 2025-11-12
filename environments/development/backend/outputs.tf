c262dcadef9192a9ec7c51b0c7c54222afec85e7
ongeziwe17 <ongeziwejunior6@gmail.com>
2025-11-12 20:49:49 +0200

    Fix Duplicate output definition for backend and frontend

--- environments/development/backend/outputs.tf
+++ environments/development/backend/outputs.tf
@@ -2,51 +2,51 @@
 # DEVELOPMENT ENVIRONMENT OUTPUTS
 # env/dev/outputs.tf
 ##############################################
-output "region" {
+output "dev_backend_region" {
   description = "Region where resources are deployed"
   value       = var.aws_region
 }
 
-output "vpc_id" {
+output "dev_backend_vpc_id" {
   description = "VPC ID for development environment"
   value       = module.network.vpc_id
 }
 
-output "public_subnets" {
+output "dev_backend_public_subnets" {
   description = "List of public subnet IDs"
   value       = module.network.public_subnet_ids
 }
 
-output "instance_public_ip" {
+output "dev_backend_instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = module.compute.instance_public_ip
 }
 
-output "dev_test_bucket" {
+output "dev_backend_dev_test_bucket" {
   description = "S3 bucket name for dev test bucket"
   value       = aws_s3_bucket.dev_test_bucket.bucket
 }
 
-output "admin_key_arn" {
+output "dev_backend_admin_key_arn" {
   description = "ARN of the admin key parameter in SSM"
   value       = module.secrets.admin_key_arn
 }
 
-output "sa_password" {
+output "dev_backend_sa_password" {
   description = "SA password parameter in SSM"
   value       = module.secrets.sa_password_arn
 }
 
-output "redis_password_arn" {
+output "dev_backend_redis_password_arn" {
   description = "Redis password parameter in SSM"
   value       = module.secrets.redis_password_arn
 }
 
-output "ghcr_token_arn" {
+output "dev_backend_ghcr_token_arn" {
   description = "GHCR Token parameter in SSM"
   value       = module.secrets.ghcr_token_arn
 }
-output "elastic_ip" {
+output "dev_backend_elastic_ip" {
   description = "Elastic IP address associated with the EC2 instance"
   value       = aws_eip.dev_app.public_ip
 }
--- environments/development/frontend/outputs.tf
+++ environments/development/frontend/outputs.tf
@@ -2,22 +2,22 @@
 # DEVELOPMENT ENVIRONMENT OUTPUTS
 # env/dev/frontend/outputs.tf
 ##############################################
-output "region" {
+output "dev_frontend_region" {
   description = "Region where resources are deployed"
   value       = var.aws_region
 }
 
-output "vpc_id" {
+output "dev_frontend_vpc_id" {
   description = "VPC ID for development environment"
   value       = module.network.vpc_id
 }
 
-output "public_subnets" {
+output "dev_frontend_public_subnets" {
   description = "List of public subnet IDs"
   value       = module.network.public_subnet_ids
 }
 
-output "instance_public_ip" {
+output "dev_frontend_instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = module.compute.instance_public_ip
 }
@@ -27,26 +27,11 @@ output "dev_frontend_test_bucket" {
   value       = aws_s3_bucket.dev_test_bucket.bucket
 }
 
-# output "admin_key_arn" {
-#   description = "ARN of the admin key parameter in SSM"
-#   value       = module.secrets.admin_key_arn
-# }
-
-# output "sa_password" {
-#   description = "SA password parameter in SSM"
-#   value       = module.secrets.sa_password_arn
-# }
-
-# output "redis_password_arn" {
-#   description = "Redis password parameter in SSM"
-#   value       = module.secrets.redis_password_arn
-# }
-
-output "ghcr_token_arn" {
+output "dev_frontend_ghcr_token_arn" {
   description = "GHCR Token parameter in SSM"
   value       = module.secrets.ghcr_token_arn
 }
-output "elastic_ip" {
+output "dev_frontend_elastic_ip" {
   description = "Elastic IP address associated with the EC2 instance"
   value       = aws_eip.dev_app.public_ip
 }
