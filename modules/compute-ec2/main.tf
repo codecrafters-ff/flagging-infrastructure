1f96af2bb30b955bf8518fde59911df2d47c628f
ongeziwe17 <ongeziwejunior6@gmail.com>
2025-11-13 00:54:01 +0200

    Use count directly on the EC2 resource

--- modules/compute-ec2/main.tf
+++ modules/compute-ec2/main.tf
@@ -61,8 +61,7 @@ resource "aws_instance" "app_server" {
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
   key_name                    = var.key_name
-  depends_on                  = var.create_key_pair ? [aws_key_pair.dev_admin] : []
-  user_data = templatefile("${path.module}/user-data.sh", {
+  user_data                   = templatefile("${path.module}/user-data.sh", {
     ENVIRONMENT_TYPE = var.environment_type
   })
 
